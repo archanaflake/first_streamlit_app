991a2e9c752accaf9b5c05045aae4e2c0d405c58
Archana <104793107+archanaflake@users.noreply.github.com>
2022-05-02 15:40:33 -0500

    Update streamlit_app.py
    loaded pandas module

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 streamlit.title('My Mom\'s New Helathy Dinner')
 
 
@@ -10,3 +10,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
